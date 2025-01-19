daa934130a4f629e1515f608d174f2cf741b7f7f
Justin <jsousa1919@gmail.com>
2025-01-19 16:23:02 -0500

    Added article page draft

--- home-page/src/views/ClioArticles.js
+++ home-page/src/views/ClioArticles.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  Chip,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
-import { Card, CardContent, Typography, Box } from "@mui/material";
+const ARTICLES = [
+  {
+    'date': '1/19/2025',
+    'path': 'Nine arts of software development.htm',
+    'title': 'Nine Arts of Software Development',
+  },
+  {
+    'date': '1/18/2025',
+    'path': 'Nine arts of software development.htm',
+    'title': 'Nine Arts of Software Development Part deux',
+  },
+];
 
-const ClioArticles = () => {
-   
-  console.log("Articles Element");
-  
+const ArticleContent = ({path}) => {
+  return path;
+}
+
+const ArticleCard = (props) => {
+  const {
+    articleData,
+  } = props;
+  if (!articleData) return null;
+  const {
+    date,
+    path,
+    title,
+  } = articleData;
+
   return (
     <Card
-        height="100%"
-        variant="outlined"
-        sx={{
-          paddingBottom: "0",
-        }}
+      variant="outlined"
+      sx={{
+        p: 0,
+        width: "100%",
+      }}
     >
       <CardContent
         sx={{
@@ -23,63 +58,133 @@ const ClioArticles = () => {
         }}
       >
         <Box
+          p={2}
+          display="flex"
           sx={{
-            display: {
-              sm: "flex",
-              xs: "block",
-            },
-            alignItems: "center",
+            flexDirection: 'column'
           }}
         >
-          <Box>
-            <Typography
-              variant="h3"
-              sx={{
-                marginTop: "5",
-                marginBottom: "0",
-              }}
-              gutterBottom
-            >
-              Software Architecture and Design Articles
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              marginLeft: "auto",
-              display: "flex",
-              mt: {
-                lg: 0,
-                xs: 2,
-              },
-            }}
-          >
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                marginLeft: "5px",
-              }}
-            >
-            </Box>
-          </Box>
+          <Typography variant="h4">{title}</Typography>
+          <Typography variant="h6" color="info">{date}</Typography>
         </Box>
-        <Box
-          sx={{
-            marginTop: "20px",
-            marginBottom: "20px",
-          }}
-        >
-        {/* --------------------------- */}
+        <Divider />
+        <Box p={2}>
+          <ArticleContent />
         </Box>
       </CardContent>
     </Card>
+  );
+};
+
+const ArticlesList = (props) => {
+  const {
+    currentArticleIdx,
+    setArticleIdx,
+  } = props;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Card
+        variant="outlined"
+        sx={{
+          p: 0,
+          width: "100%",
+        }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell
+                colSpan={2}
+              >
+                <Typography variant="h4">Articles</Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ARTICLES.map(({date, path, title}, idx) => (
+              <TableRow 
+                selected={idx === currentArticleIdx}
+                onClick={() => setArticleIdx(idx)}
+                sx={{
+                  cursor: 'pointer'
+                }}
+              >
+                <TableCell>
+                  {date}
+                </TableCell>
+                <TableCell>
+                  {title}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
+    </Box>
+  );
+};
+
+const ArticlesContainer = () => {
+  const [articleIdx, setArticleIdx] = useState(0);
+  const currentArticleData = ARTICLES[articleIdx];
+
+  return (
+    <Box
+      sx={{
+        marginLeft: "auto",
+        display: "flex",
+        mt: {
+          lg: 0,
+          xs: 2,
+          width: "100%",
+        },
+      }}
+    >
+      <ArticlesList
+        currentArticleIdx={articleIdx}
+        setArticleIdx={setArticleIdx}
+      />
+      <ArticleCard
+        articleData={currentArticleData}
+      />
+    </Box>
+  );
+}
+
+const ClioArticles = () => {
+   
+  console.log("Articles Element");
+  
+  return (
+    <Box>
+      <Box>
+        <Card
+          height="100%"
+          variant="outlined"
+        >
+          <CardContent
+            sx={{
+              p: 0,
+              '&:last-child': { pb: 0},
+            }}
+          >
+            <Typography
+              variant="h3"
+            >
+              Software Architecture and Design Articles
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box>
+        <ArticlesContainer />
+      </Box>
+    </Box>
   );
 };
 export default ClioArticles;
